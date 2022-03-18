8820d1c43587ba85389bba6cece62c6f5dbcab28
julien awon'ga <julienawon@gmail.com>
2022-03-18 21:59:22 +0100

    mise à jour dashboad form

--- views/admin/post/new.php
+++ views/admin/post/new.php
@@ -3,7 +3,40 @@
      <!-- MAIN CONTENT -->
      <div class="main-content">
           <div class="container-fluid">
-               <h1>New Article</h1>
+                    <form style="width: 500px;">
+                         <!-- Text input -->
+                         <div class="form-outline mb-4">
+                              <input type="text" id="form6Example3" class="form-control" />
+                              <label class="form-label" for="form6Example3">Company name</label>
+                         </div>
+
+                         <!-- Text input -->
+                         <div class="form-outline mb-4">
+                              <input type="text" id="form6Example4" class="form-control" />
+                              <label class="form-label" for="form6Example4">Address</label>
+                         </div>
+
+                         <!-- Email input -->
+                         <div class="form-outline mb-4">
+                              <input type="email" id="form6Example5" class="form-control" />
+                              <label class="form-label" for="form6Example5">Email</label>
+                         </div>
+
+                         <!-- Number input -->
+                         <div class="form-outline mb-4">
+                              <input type="number" id="form6Example6" class="form-control" />
+                              <label class="form-label" for="form6Example6">Phone</label>
+                         </div>
+
+                         <!-- Message input -->
+                         <div class="form-outline mb-4">
+                              <textarea class="form-control" id="form6Example7" rows="4"></textarea>
+                              <label class="form-label" for="form6Example7">Additional information</label>
+                         </div>
+
+                         <!-- Submit button -->
+                         <button type="submit" class="btn btn-primary btn-block mb-4" style="width: 100px;">Place order</button>
+               </form>
           </div>
      </div>
      <!-- END MAIN CONTENT -->
