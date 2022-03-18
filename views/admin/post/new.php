f07dc29345387e2b365ff7bce6884a4cff521323
julien awon'ga <julienawon@gmail.com>
2022-03-18 23:00:36 +0100

    mise à jour dashboad form 2

--- views/admin/post/new.php
+++ views/admin/post/new.php
@@ -3,39 +3,27 @@
      <!-- MAIN CONTENT -->
      <div class="main-content">
           <div class="container-fluid">
-                    <form style="width: 500px;">
+                    <form>
                          <!-- Text input -->
-                         <div class="form-outline mb-4">
+                         <div class="mb-5">
+                              <label class="form-label" for="form6Example3">Titre</label>
                               <input type="text" id="form6Example3" class="form-control" />
-                              <label class="form-label" for="form6Example3">Company name</label>
                          </div>
 
                          <!-- Text input -->
-                         <div class="form-outline mb-4">
-                              <input type="text" id="form6Example4" class="form-control" />
-                              <label class="form-label" for="form6Example4">Address</label>
-                         </div>
-
-                         <!-- Email input -->
-                         <div class="form-outline mb-4">
-                              <input type="email" id="form6Example5" class="form-control" />
-                              <label class="form-label" for="form6Example5">Email</label>
-                         </div>
-
-                         <!-- Number input -->
-                         <div class="form-outline mb-4">
-                              <input type="number" id="form6Example6" class="form-control" />
-                              <label class="form-label" for="form6Example6">Phone</label>
+                         <div class="mb-5">
+                              <label class="form-label" for="form6Example4">Image brand</label>
+                              <input type="file" id="form6Example4" class="form-control" />
                          </div>
 
                          <!-- Message input -->
-                         <div class="form-outline mb-4">
-                              <textarea class="form-control" id="form6Example7" rows="4"></textarea>
-                              <label class="form-label" for="form6Example7">Additional information</label>
+                         <div class="mb-5">
+                              <label class="form-label" for="form6Example7">Contenu</label>
+                              <textarea class="form-control" name="myTextarea" id="myTextarea" rows="4"></textarea>
                          </div>
 
                          <!-- Submit button -->
-                         <button type="submit" class="btn btn-primary btn-block mb-4" style="width: 100px;">Place order</button>
+                         <button type="submit" class="btn btn-primary btn-block my-5" style="width: 100px;">Publier</button>
                </form>
           </div>
      </div>
